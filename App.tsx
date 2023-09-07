4e5fbbc5d86908f86e351537be495381408c1c67
TANAY PANT <tanaypant71@gmail.com>
2023-09-07 21:50:15 +0530

    Button and onPress event

--- App.tsx
+++ App.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
 
-const name = 'Tanay';
-var email = 'tanay@mail.com';
-
-function fruit()
-{
-  return "Apple";
-}
-
 function App(): JSX.Element {
-  let age = 21;
+  const fruit = (val: string) => {
+    console.warn(val);
+  };
   return (
     <View>
-      <Text style={{fontSize: 30}}>{name}</Text>
-      <Text style={{fontSize: 30}}>{age}</Text>
-      <Text style={{fontSize: 30}}>{email}</Text>
-      <Text style={{fontSize: 30}}>{fruit()}</Text>
+      <Text style={{fontSize: 30}}>Button and onPress Event</Text>
+      <Button
+        title="On Press"
+        onPress={() => fruit('Hello Tanay')}
+        color={'green'}
+      />
     </View>
   );
 }
